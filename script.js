8eafa3fed778e6a145d7356d1267c1eddaa46871
Justyna Pruś <65462428+JustynaPrus@users.noreply.github.com>
2021-11-11 20:39:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const toDoList = [];
+
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const listItems = document.getElementsByClassName("task");
+const input = document.querySelector(".text");
+const checkbox = document.querySelector(".checkbox");
+const taskNumber = document.querySelector(".number");
+
+const removeTask = (e) => {
+  e.target.parentNode.remove();
+  const index = e.target.parentNode.dataset.key;
+  toDoList.splice(index, 1);
+  taskNumber.textContent = listItems.length;
+  renderList();
+};
+
+const addTask = (e) => {
+  e.preventDefault();
+  const titleTask = input.value;
+  if (titleTask === "") return;
+  const task = document.createElement("li");
+  task.className = "task";
+  task.innerHTML =
+    `<div><span></span>
+    <p>${titleTask}</p></div>` + `<img src="/images/icon-cross.svg">`;
+  toDoList.push(task);
+  renderList();
+
+  ul.appendChild(task);
+  input.value = "";
+  liItems = document.querySelectorAll("li.task").length;
+  taskNumber.textContent = listItems.length;
+  task.querySelector("img").addEventListener("click", removeTask);
+};
+
+const renderList = () => {
+  ul.textContent = "";
+  toDoList.forEach((toDoElement, key) => {
+    toDoElement.dataset.key = key;
+    ul.appendChild(toDoElement);
+  });
+};
+
+form.addEventListener("submit", addTask);
